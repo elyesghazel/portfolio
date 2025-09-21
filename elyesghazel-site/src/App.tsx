efc1f35a8da90616ce3b480defd4dd89b0d899e3
Elyes <gitkraken@elyesghazel.ch>
2025-09-21 21:57:39 +0200

    initial commit

---
+++ elyesghazel-site/src/App.tsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-dark text-light">
+        <div className="text-center">
+          <h1 className="fs-1 fw-semibold">Elyes Ghazel a Software Engineer</h1>
+          <p className="text-muted-foreground text-lg">
+            This website is in production
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
