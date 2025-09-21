2feaae0cdad014ca59bcfed2e825b0c322360a80
Elyes <gitkraken@elyesghazel.ch>
2025-09-21 22:24:12 +0200

    added mail

--- elyesghazel-site/src/App.tsx
+++ elyesghazel-site/src/App.tsx
@@ -7,7 +7,16 @@ function App() {
         <div className="text-center">
           <h1 className="fs-1 fw-semibold">Elyes Ghazel a Software Engineer</h1>
           <p className="text-muted-foreground text-lg">
-            This website is in production
+            I'm a software engineer and this website is currently in production.
+          </p>
+          <p className="text-muted-foreground text-lg">
+            If you have questions, feel free to contact me:{" "}
+            <a
+              href="mailto:info@elyesghazel.ch"
+              className="text-primary text-decoration-underline"
+            >
+              info@elyesghazel.ch
+            </a>
           </p>
         </div>
       </div>
