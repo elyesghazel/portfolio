e886e94552d8911defb1057690b6a92913993252
elyes <elyes@elyesghazel.ch>
2025-10-12 23:56:30 +0200

    finished home page

---
+++ elyesghazel-site/src/components/home/quickstats.ts
@@ -0,0 +1,21 @@
+import { BugIcon, MedalIcon, UserIcon } from "lucide-react";
+
+export default function quickstats() {
+  return [
+    {
+      label: "Bugs fixed",
+      value: "500+",
+      icon: BugIcon,
+    },
+    {
+      label: "Years experience",
+      value: "7+",
+      icon: MedalIcon,
+    },
+    {
+      label: "Clients satisfied",
+      value: "10+",
+      icon: UserIcon,
+    },
+  ];
+}
--- elyesghazel-site/src/pages/home/page.tsx
+++ elyesghazel-site/src/pages/home/page.tsx
@@ -1,7 +1,23 @@
+import QuickStatsElement from "../../components/home/QuickStatsElement";
+
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center h-full">
-      <h1>Centered Div</h1>
+    <div className="hero flex flex-col items-center p-5 pt-10 gap-24 w-full lg:w-[85%]">
+      <div className="title text-center">
+        <h1 className="md:text-[64px] text-[28px] font-semibold">
+          Creating <span className="text-gradient">beatiful</span> tech and web
+          solutions.
+        </h1>
+        <span className="subtitle md:text-[20px] text-[16px] w-full text-(--text-subtitle)">
+          On a journey to create technology that feels human and works
+          flawlessly.
+        </span>
+      </div>
+      <button className="px-8 md:py-3 py-2 max-w-fit text-[16px] text-center bg-(--primary-color) rounded-3xl hover:bg-(--primary-color-darker">
+        What do i offer?
+      </button>
+
+      <QuickStatsElement />
     </div>
   );
 }
